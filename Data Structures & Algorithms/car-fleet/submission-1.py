769eaab6bdd069bd079ea6066c2cafc5b099899f
nrnjn <9111993+terrinoculon@users.noreply.github.com>
2026-06-29 10:57:18 +0200

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        triplets =[(pos, vel, (target - pos)/ vel) for pos, vel in zip(position, speed)]
+        triplets = sorted(triplets, reverse = True )
+        fleets = []
+        fleets.append(triplets[0])
+        for car in triplets[1:]:
+            if car[2]>fleets[-1][2]:
+                fleets.append(car)
+        return len(fleets)
