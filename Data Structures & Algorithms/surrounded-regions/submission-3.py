a0d93a43ccf6342efa023262f777a37dfaece057
nrnjn <9111993+terrinoculon@users.noreply.github.com>
2026-07-27 10:57:48 +0200

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        R, C = len(board), len(board[-1])
+        sources = []
+        for i in range(R):
+            if board[i][0] == 'O':
+                sources.append((i,0))
+            if board[i][C-1] == 'O':
+                sources.append((i,C-1))
+        for i in range(C):
+            if board[0][i] == 'O':
+                sources.append((0,i))
+            if board[R-1][i] == 'O':
+                sources.append((R-1,i))
+        
+        seen = set()
+        q = deque(sources)
+        while q:
+            r,c = q.popleft()
+            seen.add((r,c))
+            for dr,dc in ((-1,0), (0,-1), (1,0), (0,1)):
+                nr, nc = r +dr, c + dc
+                if 0<=nr<R and 0<=nc<C and (nr,nc) not in seen and board[nr][nc]=='O':
+                    q.append((nr,nc))
+        for i in range(R):
+            for j in range(C):
+                if board[i][j] == 'O' and (i,j) not in seen:
+                    board[i][j] = 'X'
